86262e615d96e91dc6ffbf4068eaeb2067f3391a
John Spruijt <99058314@mydavinci.nl>
2020-02-14 13:59:46 +0100

    fixed bug in age script

--- age.js
+++ age.js
@@ -1,15 +1,21 @@
 //John Spruijt ©2020 
 var d = new Date();
-var year = d.getFullYear()-1;
+var year = d.getFullYear();
 var month = d.getMonth();
 var day = d.getDate();
 var age = year-2002;
 if (month == 10) {
     if (day >= 28) {
-        age++
+
+    }
+    else {
+        age--
     }
 }
-else if (month > 10) {
-    age++
+else if (month == 11) {
+    
+}
+else {
+    age--
 }
 document.getElementById("insertAge").innerHTML = age;
